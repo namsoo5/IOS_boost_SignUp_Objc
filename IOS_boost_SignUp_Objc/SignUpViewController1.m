2883e15682dd0dfba88c97b31c264ae8b3573c97
namsoo5 <are34@naver.com>
2019-08-03 15:27:56 +0900

    회원가입화면1 구현

--- IOS_boost_SignUp_Objc/SignUpViewController1.m
+++ IOS_boost_SignUp_Objc/SignUpViewController1.m
@@ -7,6 +7,7 @@
 //
 
 #import "SignUpViewController1.h"
+#import "UserInfo.h"
 
 @implementation SignUpViewController1{
     
@@ -15,17 +16,55 @@
     __weak IBOutlet UITextField *idTextField;
     __weak IBOutlet UITextField *pwTextField;
     __weak IBOutlet UITextField *pw2TextField;
+    
+    UserInfo *info;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    info = [UserInfo getInstance];
 }
 
 
 - (IBAction)nextBntClick:(UIButton *)sender {
+    
+    if([self emptyCheck]){
+        
+        //Alert생성
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"확인" message:@"빈칸이나 비밀번호를 확인해주세요" preferredStyle: UIAlertControllerStyleAlert];
+        
+        UIAlertAction * ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+        
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    }
+    
+    else{
+        info.userId = idTextField.text;
+        info.userPw = pwTextField.text;
+        info.userMemo = textView.text;
+        
+        NSLog(@"%@, %@", info.userId, info.userPw);
+    }
 }
 - (IBAction)cancelBntClick:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+//빈칸이나 비밀번호 같은지 확인
+- (Boolean)emptyCheck{
+    if(idTextField.text == nil)
+        return true;
+    if(pwTextField.text == nil)
+        return true;
+    if(pwTextField.text != pw2TextField.text){
+        return true;
+    }
+    
+    return false;
 }
 
 @end
